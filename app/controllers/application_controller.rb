32662cf1b760b05d13533ed43383b2ef4d9b7a36
Semyz Serhii <semizserhii@gmail.com>
2018-11-26 10:58:58 +0200

    fix sum_deff_prod controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+ load 'feature.rb'
+  def current_user
+    Feature.new
+  end
 end
--- app/controllers/sum_deff_prods_controller.rb
+++ app/controllers/sum_deff_prods_controller.rb
@@ -1,7 +1,10 @@
-require 'solving_math_tasks'
 class SumDeffProdsController < ApplicationController
+  attr_writer :first_number, :second_number
   def index
-     @answers = Feature.new.sum_diff_product(:first_number, :second_number)
+    @a = []
+     @answers = current_user.sum_diff_product(:first_number, :second_number)
+    @a << @answers
   end
+
 
 end
