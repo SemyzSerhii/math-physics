a798a9d230523e22102848dae197977a39e7df83
Semyz Serhii <semizserhii@gmail.com>
2018-11-30 23:48:36 +0200

    add new tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,10 @@
 class TasksController < ApplicationController
   def index
-     @answers = list.sum_diff_product( params[:first_number].to_i, params[:second_number].to_i)
+     @task1 = list.sum_diff_product( params[:first_number].to_i, params[:second_number].to_i)
+     @task2 = list.formula_solution( params[:value_x].to_i, params[:value_y].to_i)
+     @task3 = list.volume_area_cube( params[:length_of_the_edge].to_i)
+     @task4 = list.average_ar_geo_num( params[:first_number].to_i, params[:second_number].to_i)
+
   end
 
 
